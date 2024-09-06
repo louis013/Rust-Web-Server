ef5c21df2300e407e937b58abaefe22dc5b15644
louis013 <angelo.louis13@gmail.com>
2024-09-06 15:18:31 +0800

    Added structs for task, user, database

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 use actix_cors::Cors;
-use actix_web::{http::header, web, App, HttpServer, Responder, HttpResponse};
 
-use serde::{Deserialize, Serialize};
+use actix_web::{ http::header, web, App, HttpServer, Responder, HttpResponse };
+
+use serde::{ Deserialize, Serialize };
+
+use reqwest::Client as HttpClient;
+
+use async_trait::async_trait;
 
 use std::sync::Mutex;
 use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u64,
+    username: String,
+    password: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    tasks: HashMap<u64, Task>,
+    users: HashMap<u64, User>
+}
 
 fn main() {
     
