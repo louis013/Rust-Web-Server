cc3499e3414910eab78c39f653704c1c173700ee
louis013 <angelo.louis13@gmail.com>
2024-09-06 15:13:00 +0800

    Initial project setup

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use actix_cors::Cors;
+use actix_web::{http::header, web, App, HttpServer, Responder, HttpResponse};
+
+use serde::{Deserialize, Serialize};
+
+use std::sync::Mutex;
+use std::collections::HashMap;
+use std::fs;
+use std::io::Write;
+
+fn main() {
+    
+}
